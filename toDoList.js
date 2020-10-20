560fe682a7e84a2e7c2459cabb603095513964a8
SealWannaFly <Lexa.Melnicov@yandex.ru>
2020-10-20 23:01:05 +0300

    WorkingWithAForm

--- toDoList.js
+++ toDoList.js
@@ -1,8 +1,51 @@
-let ol = document.querySelector('.todolist');
-let li = ol.querySelectorAll('li');
+let list = document.querySelector('.todolist');
+let tasks = list.querySelectorAll('task');
 let height = 0;
 for (i = 0; i < 3; i++) {
-  height = height + li[i].offsetHeight;
-  console.log(height);
+  height = height + task[i].offsetHeight;
+  consliste.log(height);
 }
-ol.style.maxHeight = height+'px';
+list.style.maxHeight = height+'px';
+
+function createTask (text) {
+	let newTask = document.createElement('li');
+	newTask.classList.add('flex-container-row');
+	
+	let taskText = document.createElement('label');
+	taskText.classList.add('task-text');
+	taskTest.textContent = text;
+	newTask.appendChild(taskText);
+	
+	let doneCheckbox = document.createElement('input');
+	doneCheckbox.classList.add('task-done-checkbox');
+	doneCheckbox.setAttribute('type', 'checkbox');
+	newTask.appendChild(doneCheckbox);
+	
+	let cancelCheckbox = document.createElement('input');
+	cancelCheckbox.classList.add('task-cancel-checkbox');
+	cancelCheckbox.setAttribute('type', 'checkbox');
+	newTask.appendChild(cancelCheckbox);
+	
+	let creationDate = document.createElement('label');
+	creationDate.classList.add('task-creation-date');
+	creationDate.textContent = new Date();
+	newTask.appendChild(creationDate);
+	
+	let resultDate = document.createElement('label');
+	resultDate.classList.add('task-result-date');
+	resultDate.textContent = new Date();
+	newTask.appendChild(resultDate); 
+	
+	return newTask;
+}
+
+let addForm = document.forms.addTask;
+addForm.onsubmit = function(evt){
+	evt.preventDefault();
+	
+	let input = addForm.elements.text;
+	
+	let newTask = createTask(input);
+	
+	list.append(newTask);
+}
